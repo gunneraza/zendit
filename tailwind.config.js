82d9545a8a286449d3b5518fdb22528f5084e9b2
gunneraza <gunnera3a@gmail.com>
2021-02-15 19:25:04 +0500

    add: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      padding: '10px'
+    },
+    screens: {
+      '3xl': '1635px'
+    }
   },
   variants: {
     extend: {},
